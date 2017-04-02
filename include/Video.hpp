a652de9aaf97d70523db92ffe745ef46178d0a28
Gabriel Barbosa <gabrielfbbarbosa@gmail.com>
2017-04-02 15:20:48 -0300

    Cria classe Video

---
+++ include/Video.hpp
@@ -0,0 +1,23 @@
+#ifndef _VIDEO_HPP_
+#define _VIDEO_HPP_
+
+#include <cstdio>
+
+#include "opencv2/opencv.hpp"
+
+class Video;
+
+#include "Image.hpp"
+
+class Video : public Image {
+  public:
+    Video (const cv::String& file);
+    Video (int camIndex);
+    void Show (const cv::String & winname, bool showPainted);
+    bool IsValid ();
+    bool NextFrame ();
+  private:
+    cv::VideoCapture cap;
+};
+
+#endif //_VIDEO_HPP_
