97aa0b98a04f3492a12704f23b718a83301c3d97
Gabriel Barbosa <gabrielfbbarbosa@gmail.com>
2017-05-05 01:49:57 -0300

    Faz uma main minima para teste do carregamento das imagens

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
-#include <stdio.h>
+#include <cstdio>
+#include <string>
 
 #include "opencv2/opencv.hpp"
 
 #include "CMakeVars.hpp"
+#include "Image.hpp"
 
 using namespace cv;
+
+const float df = 25.0; // mm
+const float baseline = 120.0; // mm
 
 int main(int argc, char** argv )
 {
@@ -14,16 +19,18 @@ int main(int argc, char** argv )
         return -1;
     }
 
-    Mat image;
-    image = imread( argv[1], 1 );
+    Image imL = Image (std::string(argv[1]) + "L.png");
+    Image imR = Image (std::string(argv[1]) + "R.png");
 
-    if ( !image.data )
+    if ( !imL.IsValid() || !imR.IsValid() )
     {
         printf("No image data \n");
         return -1;
     }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
+    namedWindow("Display Left", WINDOW_AUTOSIZE );
+    namedWindow("Display Right", WINDOW_AUTOSIZE );
+    imL.Show ("Display Left");
+    imR.Show ("Display Right");
 
     waitKey(0);
 
