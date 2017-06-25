a103b5bf1c81e49181e73e2e2d67ead27dffde26
Gabriel Barbosa <gabrielfbbarbosa@gmail.com>
2017-06-25 15:33:22 -0300

    Captura imagens de arquivo de video

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,34 @@
 #include <stdio.h>
 
 #include "opencv2/opencv.hpp"
+#include <string>
 
 #include "CMakeVars.hpp"
 
 using namespace cv;
 
-int main(int argc, char** argv )
-{
-    if ( argc != 2 )
-    {
-        printf("usage: cv_test <Image_Path>\n");
-        return -1;
-    }
+int main(int argc, char** argv ) {
+	if ( argc != 2 ) {
+		printf("usage: cv_test <Image_Path>\n");
+		return -1;
+	}
 
-    Mat image;
-    image = imread( argv[1], 1 );
+	VideoCapture cap( argv[1] );
 
-    if ( !image.data )
-    {
-        printf("No image data \n");
-        return -1;
-    }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
+	int key = 255;
+	do {
+		Mat frame;
+		cap.read(frame);
 
-    waitKey(0);
+		if ( frame.empty() ) {
+			printf("No frame data \n");
+			return -1;
+		}
 
-    return 0;
+		imshow("Original Video", frame);
+
+		key = waitKey(1);
+	} while( 255 == key );
+
+	return 0;
 }
